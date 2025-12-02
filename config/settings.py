c5ab99041f8d4e783ed77c9df9d958f0a8d8c478
Вавуло Светлана <sveta250206@gmail.com>
2025-12-02 21:32:28 +0300

    Дополнительная небольшая настройка

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,12 +40,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'drf_yasg',
+    'corsheaders',
     "habits",
     "users",
     "tg_bot",
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -110,9 +116,16 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+CORS_ALLOWED_ORIGINS = [ 'http://localhost:8000']
+CSRF_TRUSTED_ORIGINS = [ "https://read-and-write.example.com"]
+CORS_ALLOW_ALL_ORIGINS = True
 
-TIME_ZONE = "UTC"
+CELERY_BROKER_URL = 'memory://'
+CELERY_RESULT_BACKEND = 'django-db'
+
+LANGUAGE_CODE = "ru-ru"
+
+TIME_ZONE = "Europe/Moscow"
 
 USE_I18N = True
 

e0dd6b9cac047a806892cab2cb96285af75dd0c1
Вавуло Светлана <sveta250206@gmail.com>
2025-12-03 20:46:31 +0300

    Настройка приложения habits, новый функционал описан в README.md

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,35 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Habit Tracker API",
+        default_version="v1",
+        description="API для трекера привычек по книге 'Атомные привычки'",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@habits.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include("habits.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
